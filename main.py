ff56c809646fc3faf3e573351933952e7d1f1ba4
이윤호 <Godmelon0221@gmail.com>
2026-01-14 14:24:57 +0900

    :q!

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("400x400")
-        self.title("CustomTkinter")
+        self.title("MeloniMP3player")
         self.resizable(False, False)
-        #self.iconbitmap("icon.ico")
+        self.iconbitmap("img/icon.ico")
         
         
         
